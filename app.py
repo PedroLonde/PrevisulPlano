c575536cd97904873ca596015695c7c748f52f2a
PedroLonde <149105949+PedroLonde@users.noreply.github.com>
2024-06-07 10:48:18 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,25 +4,19 @@ import os
 
 def main():
     st.title("Processamento de Relatórios Excel")
-    
+
     uploaded_file_nov = st.file_uploader("Escolha o arquivo Relatório Novo", type=["xlsx"], key="nov")
     uploaded_file_pend = st.file_uploader("Escolha o arquivo Pendentes Alteração", type=["xlsx"], key="pend")
 
-    if os.path.exists("RelatorioGeral.xlsx"):
-        ant = pd.read_excel("RelatorioGeral.xlsx")
-    else:
-        st.error("O arquivo RelatorioGeral.xlsx não foi encontrado. Certifique-se de que ele exista na pasta.")
-        return
-
     if uploaded_file_nov and uploaded_file_pend:
         nov = pd.read_excel(uploaded_file_nov, header=1)
-        dados_pendentes = pd.read_excel(uploaded_file_pend)
+        ant = pd.read_excel("RelatorioGeral.xlsx") if os.path.exists("RelatorioGeral.xlsx") else pd.DataFrame()
 
         nov = nov.drop(['Data de Cadastro', 'Data de Adesão', 'Data de Ativação', 
                         'Tipo de Benefício', 'Sequencial de Benefício','Sequencial','Cartão de Desconto','Identidade','Unidade', 'Dia de Vencimento', 
                         'Situação', 'Consultor', 'Bairro', 'Cidade', 'UF', 'Telefone', 
                         'Email', 'Dados Adicionais'], axis=1)
-        
+
         nov = nov.dropna(subset=['CPF/CNPJ'])
         nov.reset_index(drop=True, inplace=True)
 
@@ -35,7 +29,9 @@ def main():
                         idade_antiga = ant_beneficiario.iloc[0]['Idade']
                         if idade_antiga > 69:
                             nov.loc[x, 'Idade'] = idade_antiga
-        
+
+        dados_pendentes = pd.read_excel(uploaded_file_pend)
+
         for index, row in nov.iterrows():
             if pd.isnull(row['Sexo']):
                 beneficiario = row['Nome do Beneficiário']
@@ -52,7 +48,7 @@ def main():
             elif nov.loc[x, 'Tipo de Beneficiário'] == 'Dependente':
                 nov.loc[x, 'Titular'] = nov.loc[x-1, 'Titular']
             x += 1
-        
+
         x = 0
         while x < len(nov):
             if nov.loc[x, 'Tipo de Beneficiário'] == 'Titular':
@@ -98,7 +94,10 @@ def main():
         nov = nov.drop(['Tipo de Beneficiário','Parentesco','Idade','Titular'], axis=1)
         nov['Data de Nascimento'] = pd.to_datetime(nov['Data de Nascimento']).dt.strftime('%d/%m/%Y')
 
-        Pleno = nov[nov['Plano'].str.contains('Pleno')].drop('Plano', axis=1)
+        nov.to_excel("RelatorioGeral.xlsx", index=False)
+        Pleno = nov[nov['Plano'].str.contains('Pleno')]
+        Pleno = Pleno.drop('Plano', axis=1)
+
         novos_dados = {
             'Nome do Beneficiário': ['SOLANGE MORAES LONDE', 'MAURICO BATISTA LONDE'],
             'Sexo': ['Feminino', 'Masculino'],
@@ -107,16 +106,58 @@ def main():
             'Endereço': ['Rua Lirio Montanhes, 66 Casa', 'Rua Marcio Garcia,30 Casa'],
             'CEP': ['30555-180', '34003-074']
         }
+
         novos_df = pd.DataFrame(novos_dados)
         Pleno = pd.concat([Pleno, novos_df], ignore_index=True)
-        
-        # Salvar os DataFrames atualizados
-        nov.to_excel("RelatorioGeral.xlsx", index=False)
-        Pleno.to_excel("Pleno.xlsx", index=False)
+        Plus = nov[nov['Plano'].str.contains('Plus')].drop('Plano', axis=1)
+        Vital = nov[nov['Plano'].str.contains('Vital')].drop('Plano', axis=1)
+        Essencial = nov[nov['Plano'].str.contains('Essencial')].drop('Plano', axis=1)
 
-        st.success("Processamento concluído. Arquivos salvos.")
+        Essencial.to_excel("12009820007990.xlsx", index=False)
+        Plus.to_excel("12009820007976.xlsx", index=False)
+        Vital.to_excel("12009820007972.xlsx", index=False)
+        Pleno.to_excel("12009820007974.xlsx", index=False)
+
+        conple = 0
+        conplu = 0
+        convit = 0
+        coness = 0
+
+        for x in range(len(nov)):
+            if nov.loc[x,'Plano'] == 'Pleno':
+                conple += 1
+            elif nov.loc[x,'Plano'] == 'Plus':
+                conplu += 1
+            elif nov.loc[x,'Plano'] == 'Vital':
+                convit += 1
+            elif nov.loc[x,'Plano'] == 'Essencial':
+                coness += 1
+
+        total = convit + conple + conplu + coness
+
+        valvit = convit * 3.01
+        valple = conple * 1.73
+        valplu = conplu * 3.41
+        valess = coness * 1.08
+
+        valtotal = valvit + valple + valplu + valess
+
+        dados = {
+            'Vital': {'Quantidade': convit, 'Valor Total': valvit},
+            'Pleno': {'Quantidade': conple, 'Valor Total': valple},
+            'Plus': {'Quantidade': conplu, 'Valor Total': valplu},
+            'Essencial': {'Quantidade': coness, 'Valor Total': valess},
+            'Total': {'Quantidade': total, 'Valor Total': valtotal}
+        }
+
+        df = pd.DataFrame(dados)
+
+        st.write("Processamento concluído. Os arquivos foram salvos.")
         st.download_button("Download RelatorioGeral.xlsx", data=open("RelatorioGeral.xlsx", "rb").read(), file_name="RelatorioGeral.xlsx")
-        st.download_button("Download Pleno.xlsx", data=open("Pleno.xlsx", "rb").read(), file_name="Pleno.xlsx")
+        st.download_button("Download 12009820007990.xlsx", data=open("12009820007990.xlsx", "rb").read(), file_name="12009820007990.xlsx")
+        st.download_button("Download 12009820007976.xlsx", data=open("12009820007976.xlsx", "rb").read(), file_name="12009820007976.xlsx")
+        st.download_button("Download 12009820007972.xlsx", data=open("12009820007972.xlsx", "rb").read(), file_name="12009820007972.xlsx")
+        st.download_button("Download 12009820007974.xlsx", data=open("12009820007974.xlsx", "rb").read(), file_name="12009820007974.xlsx")
 
 if __name__ == "__main__":
     main()
